69a8446fe7bf0b7d7c4df7bb9df1381dacda2db2
jonko0493 <jonko0493@protonmail.com>
2023-08-04 03:57:28 -0700

    Attempt to narrow the feed a bit

--- server/data_filter.py
+++ server/data_filter.py
@@ -3,34 +3,32 @@ from atproto import models
 from server.logger import logger
 from server.database import db, Post
 from server.anilist_scraper import gaynimes
+from english_words import get_english_words_set
 
 def operations_callback(ops: dict) -> None:
-    # Here we can filter, process, run ML classification, etc.
-    # After our feed alg we can save posts into our DB
-    # Also, we should process deleted posts to remove them from our DB and keep it in sync
-
-    # for example, let's create our custom feed that will contain all posts that contains alf related text
-
+    english_words = get_english_words_set(['web2'], alpha=True, lower=True)
     posts_to_create = []
     for created_post in ops['posts']['created']:
         record = created_post['record']
 
-        if any(gay in record.text.lower() for gay in gaynimes):
-            reply_parent = None
-            if record.reply and record.reply.parent.uri:
-                reply_parent = record.reply.parent.uri
+        for gay in gaynimes:
+            if gay in record.text.lower() and (gay not in english_words or ("anime" in record.text.lower() or "manga" in record.text.lower())):
+                print(gay)
+                reply_parent = None
+                if record.reply and record.reply.parent.uri:
+                    reply_parent = record.reply.parent.uri
 
-            reply_root = None
-            if record.reply and record.reply.root.uri:
-                reply_root = record.reply.root.uri
+                reply_root = None
+                if record.reply and record.reply.root.uri:
+                    reply_root = record.reply.root.uri
 
-            post_dict = {
-                'uri': created_post['uri'],
-                'cid': created_post['cid'],
-                'reply_parent': reply_parent,
-                'reply_root': reply_root,
-            }
-            posts_to_create.append(post_dict)
+                post_dict = {
+                    'uri': created_post['uri'],
+                    'cid': created_post['cid'],
+                    'reply_parent': reply_parent,
+                    'reply_root': reply_root,
+                }
+                posts_to_create.append(post_dict)
 
     posts_to_delete = [p['uri'] for p in ops['posts']['deleted']]
     if posts_to_delete:
