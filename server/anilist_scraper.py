843310c5e8d8fa14c5b8eb25a20055e52e4fd21b
jonko0493 <jonko0493@protonmail.com>
2023-08-06 12:03:22 -0700

    Remove some of the more general hits

--- server/anilist_scraper.py
+++ server/anilist_scraper.py
@@ -31,15 +31,13 @@ class WeightedAttribute:
             "item": self.item,
             "weight": self.weight,
         }
-    def from_db(entry):
-        return WeightedAttribute(entry['item'], entry['weight'])
 
 class Character:
     def __init__(self, character):
-        self.name = WeightedAttribute(None, 0.0) if character['name']['full'] is None else WeightedAttribute(character['name']['full'], 1.0)
-        self.name_first = WeightedAttribute(None, 0.0) if character['name']['first'] is None else WeightedAttribute(character['name']['first'], 1.0)
-        self.name_last = WeightedAttribute(None, 0.0) if character['name']['last'] is None else WeightedAttribute(character['name']['last'], 1.0)
-        self.name_pref = WeightedAttribute(None, 0.0) if character['name']['userPreferred'] is None else WeightedAttribute(character['name']['userPreferred'], 1.0)
+        self.name = WeightedAttribute(None, 0.0) if character['name']['full'] is None else WeightedAttribute(character['name']['full'].lower(), 1.0)
+        self.name_first = WeightedAttribute(None, 0.0) if character['name']['first'] is None else WeightedAttribute(character['name']['first'].lower(), 1.0)
+        self.name_last = WeightedAttribute(None, 0.0) if character['name']['last'] is None else WeightedAttribute(character['name']['last'].lower(), 1.0)
+        self.name_pref = WeightedAttribute(None, 0.0) if character['name']['userPreferred'] is None else WeightedAttribute(character['name']['userPreferred'].lower(), 1.0)
     def db(self):
         return {
             "name": self.name.db(),
@@ -47,13 +45,6 @@ class Character:
             "name_last": self.name_last.db(),
             "name_pref": self.name_pref.db(),
         }
-    def from_db(entry):
-        character = Character()
-        character.name = WeightedAttribute.from_db(entry['name'])
-        character.name_first = WeightedAttribute.from_db(entry['name_first'])
-        character.name_last = WeightedAttribute.from_db(entry['name_last'])
-        character.name_pref = WeightedAttribute.from_db(entry['name_pref'])
-        return character
 
 class Gaynime:
     def __init__(self, media):
@@ -61,7 +52,7 @@ class Gaynime:
         self.title_romaji =  WeightedAttribute(None, 0.0) if media['title']['romaji'] is None else WeightedAttribute(media['title']['romaji'].strip().lower(), 1.0)
         self.title_english = WeightedAttribute(None, 0.0) if media['title']['english'] is None else WeightedAttribute(media['title']['english'].strip().lower(), 1.0)
         self.title_native = WeightedAttribute(None, 0.0) if media['title']['native'] is None else WeightedAttribute(media['title']['native'].strip().lower(), 1.0)
-        self.hashtags = [WeightedAttribute(None, 0.0)] if media['hashtag'] is None else [WeightedAttribute(hashtag.strip()[1:], 1.0) for hashtag in media['hashtag'].split(' ')]
+        self.hashtags = [WeightedAttribute(None, 0.0)] if media['hashtag'] is None else [WeightedAttribute(hashtag.strip()[1:].lower(), 1.0) for hashtag in media['hashtag'].split(' ')]
         self.synonyms = [WeightedAttribute(synonym.strip().lower(), 1.0) for synonym in media['synonyms'] if len(synonym) > 2]
         self.entities = []
         self.characters = []
@@ -69,7 +60,7 @@ class Gaynime:
             nlp_desc = sp_en(media['description'])
             for ent in nlp_desc.ents:
                 if ent.label_ != 'CARDINAL' and ent.label_ != 'DATE' and ent.label_ != 'MONEY' and ent.label_ != 'TIME' and ent.label_ != 'PERCENT' and ent.label_ != 'QUANTITY' and ent.label_ != 'ORDINAL' and ent.text not in [item.item for item in self.entities]:
-                    self.entities.append(WeightedAttribute(ent.text, 1.0))
+                    self.entities.append(WeightedAttribute(ent.text.lower(), 1.0))
             for character in media['characters']['nodes']:
                 self.characters.append(Character(character))
     def db(self):
@@ -83,16 +74,6 @@ class Gaynime:
             "entities": [ent.db() for ent in self.entities],
             "characters": [character.db() for character in self.characters]
         }
-    def from_db(entry):
-        gaynime = Gaynime()
-        gaynime.id = entry['id']
-        gaynime.title_romaji = WeightedAttribute.from_db(entry['title_romaji'])
-        gaynime.title_english = WeightedAttribute.from_db(entry['title_english'])
-        gaynime.title_native = WeightedAttribute.from_db(entry['title_native'])
-        gaynime.hashtags = [WeightedAttribute.from_db(hashtag) for hashtag in entry['hashtags']]
-        gaynime.synonyms = [WeightedAttribute.from_db(synonym) for synonym in entry['synonyms']]
-        gaynime.entities = [WeightedAttribute.from_db(entity) for entity in entry['entities']]
-        gaynime.characters = [Character.from_db(character) for character in entry['characters']]
 
 def scrape():
     tags_to_scrape = [ "Yuri", "Boys' Love" ]
--- server/data_filter.py
+++ server/data_filter.py
@@ -25,54 +25,58 @@ def operations_callback(ops: dict) -> None:
         if record.langs is not None and 'en' in record.langs: # (gay in [ent.text.lower() for ent in tweet_en.ents if ent.label_ == 'WORK_OF_ART' or ent.label_ == 'PRODUCT'] or (gay in [token.text.lower() for token in tweet_en if token.pos_ == 'PROPN' and not token.is_oov and len(token.text) > 2]))
             tweet_en = sp_en(record.text)
             text_ents = [ent.text.lower() for ent in tweet_en.ents if ent.label_ != 'CARDINAL' and ent.label_ != 'DATE' and ent.label_ != 'MONEY' and ent.label_ != 'TIME' and ent.label_ != 'PERCENT' and ent.label_ != 'QUANTITY' and ent.label_ != 'ORDINAL']
-        for gay in gaynimes.find():
-            weight = 0
-            if gay['title_romaji']['item'] is not None and gay['title_romaji']['item'] in tweet_en.text.lower():
-                weight += gay['title_romaji']['weight']
-                logger.info(f"Found {gay['title_romaji']}, new weight {weight}")
-            if gay['title_english']['item'] is not None and gay['title_english']['item'] in tweet_en.text.lower():
-                weight += gay['title_english']['weight']
-                logger.info(f"Found {gay['title_english']}, new weight {weight}")
-            for synonym in gay['synonyms']:
-                if synonym['item'] is not None and synonym['item'].lower() in tweet_en.text.lower():
-                    weight += synonym['weight']
-                    logger.info(f"Found {synonym} new weight {weight}")
-            for ent in gay['entities']:
-                if ent['item'] is not None and ent['item'].lower() in text_ents:
-                    weight += ent['weight']
-                    logger.info(f"Found {ent}, new weight {weight}")
-            for character in gay['characters']:
-                if character['name']['item'] is not None and character['name']['item'].lower() in text_ents:
-                    weight += character['name']['weight']
-                    logger.info(f"Found {character['name']}, new weight {weight}")
-                if character['name_first']['item'] is not None and character['name_first']['item'].lower() in text_ents:
-                    weight += character['name_first']['weight']
-                    logger.info(f"Found {character['name_first']}, new weight {weight}")
-                if character['name_last']['item'] is not None and character['name_last']['item'].lower() in text_ents:
-                    weight += character['name_last']['weight']
-                    logger.info(f"Found {character['name_last']}, new weight {weight}")
-                if character['name_pref']['item'] is not None and character['name_pref']['item'].lower() in text_ents:
-                    weight += character['name_pref']['weight']
-                    logger.info(f"Found {character['name_pref']}, new weight {weight}")
-                
-                if weight > weight_threshold:
-                    logger.info(f'Added record containing "{gay}"')
-                    reply_parent = None
-                    if record.reply and record.reply.parent.uri:
-                        reply_parent = record.reply.parent.uri
+            for gay in gaynimes.find():
+                weight = 0
+                if gay['title_romaji']['item'] is not None and ((len(gay['title_romaji'].strip().split(' ')) > 1 and gay['title_romaji']['item'] in record.text.lower()) or gay['title_romaji']['item'] in [token.lower() for token in tweet_en]):
+                        weight += gay['title_romaji']['weight']
+                        logger.info(f"Found {gay['title_romaji']}, new weight {weight}")
+                if gay['title_english']['item'] is not None and ((len(gay['title_english'].strip().split(' ')) > 1 and gay['title_english']['item'] in record.text.lower()) or gay['title_english']['item'] in [token.lower() for token in tweet_en]):
+                    weight += gay['title_english']['weight']
+                    logger.info(f"Found {gay['title_english']}, new weight {weight}")
+                for hashtag in gay['hashtags']:
+                    if hashtag['item'] is not None and hashtag['item'].lower() in record.text.lower():
+                        weight += hashtag['weight']
+                        logger.info(f"Found {hashtag}, new weight {weight}")
+                for synonym in gay['synonyms']:
+                    if synonym['item'] is not None and (synonym['item'].lower() in text_ents or synonym['item'].lower() in [token.lower() for token in tweet_en if token.pos_ == 'NOUN' or token.pos_ == 'PROPN']):
+                        weight += synonym['weight']
+                        logger.info(f"Found {synonym} new weight {weight}")
+                for ent in gay['entities']:
+                    if ent['item'] is not None and ent['item'].lower() in text_ents:
+                        weight += ent['weight']
+                        logger.info(f"Found {ent}, new weight {weight}")
+                for character in gay['characters']:
+                    if character['name']['item'] is not None and character['name']['item'].lower() in text_ents:
+                        weight += character['name']['weight']
+                        logger.info(f"Found {character['name']}, new weight {weight}")
+                    if character['name_first']['item'] is not None and character['name_first']['item'].lower() in text_ents:
+                        weight += character['name_first']['weight']
+                        logger.info(f"Found {character['name_first']}, new weight {weight}")
+                    if character['name_last']['item'] is not None and character['name_last']['item'].lower() in text_ents:
+                        weight += character['name_last']['weight']
+                        logger.info(f"Found {character['name_last']}, new weight {weight}")
+                    if character['name_pref']['item'] is not None and character['name_pref']['item'].lower() in text_ents:
+                        weight += character['name_pref']['weight']
+                        logger.info(f"Found {character['name_pref']}, new weight {weight}")
+                    
+                    if weight > weight_threshold:
+                        logger.info(f'Added record containing "{gay}"')
+                        reply_parent = None
+                        if record.reply and record.reply.parent.uri:
+                            reply_parent = record.reply.parent.uri
 
-                    reply_root = None
-                    if record.reply and record.reply.root.uri:
-                        reply_root = record.reply.root.uri
+                        reply_root = None
+                        if record.reply and record.reply.root.uri:
+                            reply_root = record.reply.root.uri
 
-                    post_dict = {
-                        'uri': created_post['uri'],
-                        'cid': created_post['cid'],
-                        'reply_parent': reply_parent,
-                        'reply_root': reply_root,
-                    }
-                    posts_to_create.append(post_dict)
-                    break
+                        post_dict = {
+                            'uri': created_post['uri'],
+                            'cid': created_post['cid'],
+                            'reply_parent': reply_parent,
+                            'reply_root': reply_root,
+                        }
+                        posts_to_create.append(post_dict)
+                        break
 
     posts_to_delete = [p['uri'] for p in ops['posts']['deleted']]
     if posts_to_delete:
