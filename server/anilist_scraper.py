75be3e93f3c5487992277bc9eed018c2bbed076c
jonko0493 <jonko0493@protonmail.com>
2023-08-12 16:36:39 -0700

    Update scraper to account for None names

--- server/anilist_scraper.py
+++ server/anilist_scraper.py
@@ -27,10 +27,14 @@ client = Client(transport=transport, fetch_schema_from_transport=True)
 
 class Character:
     def __init__(self, character):
-        self.name = character['name']['full']
-        self.name_first = character['name']['first'].lower()
-        self.name_last = character['name']['last'].lower()
-        self.name_pref = character['name']['userPreferred']
+        if character['name']['full'] is not None:
+            self.name = character['name']['full']
+        if character['name']['first'] is not None:
+            self.name_first = character['name']['first'].lower()
+        if character['name']['last'] is not None:
+            self.name_last = character['name']['last'].lower()
+        if character['name']['userPreferred'] is not None:
+            self.name_pref = character['name']['userPreferred']
     def db(self):
         return {
             "name": self.name,
